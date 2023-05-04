41ea13d558cc182d7562904813839c1e9bca3750
Jan Schulte <janschul@cisco.com>
2023-05-04 15:40:14 -0400

    add test

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use fake::faker::lorem::en::*;
 use fake::Fake;
 
+fn produce_words() -> String {
+    let words: Vec<String> = Words(3..5).fake();
+    words.join(",")
+}
+
 fn main() {
     loop {
-        let words: Vec<String> = Words(3..5).fake();
-        println!("words {:?}", words);
+        let words = produce_words();
+        println!("{}", words);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::produce_words;
+
+    #[test]
+    fn test_produces_words() {
+        let words = produce_words();
+        assert!(words.len() > 0, "Expected concatenated word list, got empty string");
+    }
+}
+
 
